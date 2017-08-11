2939bda8d32a6c271ce12331f544014707243cc3
Behnam Emamian <emamian@persianprocess.com>
2017-08-11 11:34:32 +1000

    Init

---
+++ Solution/Resources/Website/Install/App_LocalResources/Resx2Xlf.bat
@@ -0,0 +1,4 @@
+rem ResourceToXliff .\ en-US Installwizard.aspx.resx Installwizard.aspx.fa-IR.resx
+ResourceToXliff .\ en-US Installwizard.aspx.resx Installwizard.aspx.fa-IR.resx
+
+pause
---
+++ Solution/Resx2Xlf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Resx2Xlf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

e8aec9a90d2158f94c4382ef015ecda5a103432b
Behnam Emamian <emamian@persianprocess.com>
2017-08-11 15:27:49 +1000

    Generating all XLF files

--- Solution/Resx2Xlf/Program.cs
+++ Solution/Resx2Xlf/Program.cs
@@ -6,27 +6,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Multilingual.Utilities;
+using System.IO;
 
 namespace Resx2Xlf
 {
     class Program
     {
+        static DirectoryInfo SourceDir = new DirectoryInfo(@"D:\GitHub\Dnn.Platform.Persian\Solution\DNN\Website_v9.1.1\");
+        static DirectoryInfo DestinationDir = new DirectoryInfo(@"D:\GitHub\Dnn.Platform.Persian\Solution\Resources\Website\");
+
         static void Main(string[] args)
         {
-            var SourcePath = @"D:\GitHub\Dnn.Platform.Persian\Solution\DNN\Website_v9.1.1\";
-            var DestinationPath = @"D:\GitHub\Dnn.Platform.Persian\Solution\Resources\Website\";
+            ConvertResx2Xlf(SourceDir);
+        }
 
-            
+        static void ConvertResx2Xlf(DirectoryInfo Path)
+        {
+            foreach (var File in Path.GetFiles("*.resx"))
+            {
+                var RelativeFilePath = File.FullName.Replace(SourceDir.FullName, "").Replace(File.Extension, "");
 
+                var xliffSources = XliffDocument.ReadFromResource(SourceDir.FullName + RelativeFilePath + ".resx", new CultureInfo("en-US"), string.Empty, ResourceType.Resx);
+                xliffSources[0].FileInfos[0].TargetCulture = new CultureInfo("fa-IR");
+                xliffSources[0].Save(DestinationDir.FullName + RelativeFilePath + ".fa-IR.xlf");
+            }
 
-            var ResourceFilePath = @"D:\GitHub\Dnn.Platform.Persian\Solution\DNN\Website_v9.1.1\Install\App_LocalResources\Installwizard.aspx.resx";
-
-            var xliffSources = XliffDocument.ReadFromResource(ResourceFilePath, new CultureInfo("en-US"), string.Empty, ResourceType.Resx);
-
-            //xliffSources[0].SaveAs(@"D:\GitHub\Dnn.Platform.Persian\Solution\Resources\Website\Install\App_LocalResources\Installwizard.aspx.xlf", ResourceType.Xliff20);
-            xliffSources[0].FileInfos[0].TargetCulture = new CultureInfo("fa-IR");
-            xliffSources[0].Save(@"D:\GitHub\Dnn.Platform.Persian\Solution\Resources\Website\Install\App_LocalResources\Installwizard.aspx.xlf");
-
+            foreach (var Dir in Path.GetDirectories())
+                ConvertResx2Xlf(Dir);
         }
     }
 }
